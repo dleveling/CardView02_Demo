4dde5d7c26f719013932963c9973fcd4494429b6
dleveling <devleveling199x@gmail.com>
2018-04-13 21:01:26 +0700

    Initial commit

---
+++ app/src/main/java/com/demotxt/myapp/recyclerview/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.demotxt.myapp.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/demotxt/myapp/recyclerview/MainActivity.java
@@ -0,0 +1,48 @@
+package com.demotxt.myapp.recyclerview;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    List<Book> lstBook ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);*/
+
+        lstBook = new ArrayList<>();
+        lstBook.add(new Book("The Vegitarian","Categorie Book","Description book",R.drawable.thevigitarian));
+        lstBook.add(new Book("The Wild Robot","Categorie Book","Description book",R.drawable.thewildrobot));
+        lstBook.add(new Book("Maria Semples","Categorie Book","Description book",R.drawable.mariasemples));
+        lstBook.add(new Book("The Martian","Categorie Book","Description book",R.drawable.themartian));
+        lstBook.add(new Book("He Died with...","Categorie Book","Description book",R.drawable.hediedwith));
+
+
+        RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_id);
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,lstBook);
+        myrv.setLayoutManager(new GridLayoutManager(this,3));
+        myrv.setAdapter(myAdapter);
+
+
+
+    }
+}
